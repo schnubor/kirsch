16b626d79d15d6e73065d2d3691711413e77d757
Christian Korndoerfer <schnuppser@gmail.com>
2019-02-14 10:46:11 +0100

    fixed collapse

--- templates/renderRecht.php
+++ templates/renderRecht.php
@@ -1,11 +1,11 @@
 <?php
-    function renderRecht($recht, $title)
+    function renderRecht($recht, $parent)
     {
         $id = slugify($recht->title);
 
         echo "<div>";
         echo "\t<h4 class=\"collapse-button\" data-toggle=\"collapse\" data-target=\"#" . $id . "\" aria-expanded=\"false\" aria-controls=\"" . $id . "\">" . $recht->title . "</h4>";
-        echo "\t<div class=\"collapse\" id=\"" . $id . "\" data-parent=\"#" . $title . "\">";
+        echo "\t\t<div class=\"collapse\" id=\"" . $id . "\" data-parent=\"#" . $parent . "\">";
         echo "\t\t<p class=\"collapse-content\">" . $recht->description . "</p>";
         echo "\t</div>";
         echo "</div>";
--- templates/renderRechtsgebiet.php
+++ templates/renderRechtsgebiet.php
@@ -3,13 +3,13 @@
 
     function renderRechtsgebiet($rechtsgebiet)
     {
-        echo "<div class=\"col-md-6\" id=\"" . $rechtsgebiet->title . "\">";
+        echo "<div class=\"col-md-6\" id=\"" . slugify($rechtsgebiet->title) . "\">";
         echo "<h3>" . $rechtsgebiet->title . "</h3>";
         echo "<p>" . $rechtsgebiet->description . "</p>";
         echo "<hr>";
 
         foreach ($rechtsgebiet->rechte as $recht) {
-            renderRecht($recht, $rechtsgebiet->title);
+            renderRecht($recht, slugify($rechtsgebiet->title));
         }
 
         echo "</div>";
