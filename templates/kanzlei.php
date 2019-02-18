30b4e820af29ef990d04ace839b5a5773eac2560
Christian Korndoerfer <schnuppser@gmail.com>
2019-02-18 11:56:05 +0100

    added kanzlei

--- templates/kanzlei.php
+++ templates/kanzlei.php
@@ -1,5 +1,21 @@
+<?php
+    $entry = $contentfulClient->getEntry('3OnlyaHzr3fg21xhbLa4zU');
+    $renderer = new \Contentful\RichText\Renderer();
+?>
+
 <section id="kanzlei">
     <div class="container">
-        <h2 class="title">Die Kanzlei</h2>
+        <h2 class="title"><?php echo $entry->title ?></h2>
+
+        <div class="row content">
+            <div class="col-md-6">
+                <?php echo $renderer->render($entry->get('leftColumn')); ?>
+            </div>
+            <div class="col-md-6">
+                <div class="list animated">
+                    <?php echo $renderer->render($entry->get('rightColumn')); ?>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
--- templates/rechtsgebiete.php
+++ templates/rechtsgebiete.php
@@ -11,7 +11,7 @@ try {
 
 <section id="rechtsgebiete">
     <div class="container">
-        <h2 class="title">Rechtsgebiete</h2>
+        <h2 class="title white">Rechtsgebiete</h2>
         <div class="row content">
             <?php
             foreach ($rechtsgebiete as $rechtsgebiet) {
