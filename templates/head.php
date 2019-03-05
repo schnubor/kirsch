2b2c0597f36f1b38437e41fad0d4b273f8259e7d
schnubor <info@chko.org>
2019-03-05 13:54:51 +0100

    verify

--- templates/head.php
+++ templates/head.php
@@ -8,5 +8,6 @@
     <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bower_components/components-font-awesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="/assets/css/compiled.css">
+    <meta name="google-site-verification" content="7BeGA8diKM9YqKa5tEsnkVcVESqihtG8m8jKA1zgWHw" />
     <link href="https://fonts.googleapis.com/css?family=PT+Serif:700|Roboto:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
