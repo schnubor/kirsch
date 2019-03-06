d55d0ffc3b702039cc249f28bf94a7074851075f
schnubor <info@chko.org>
2019-03-06 20:07:04 +0100

    rechte are now rich text

--- templates/renderRecht.php
+++ templates/renderRecht.php
@@ -2,11 +2,13 @@
     function renderRecht($recht, $parent)
     {
         $id = slugify($recht->title);
+        $renderer = new \Contentful\RichText\Renderer();
+        $content = '<div class="collapse-content">' . nl2br($renderer->render($recht->get('content'))) . '</div>';
 
         echo "<div>";
         echo "\t<h4 class=\"collapse-button collapsed\" data-toggle=\"collapse\" data-target=\"#" . $id . "\" aria-expanded=\"false\" aria-controls=\"" . $id . "\"><i class=\"fas fa-chevron-right\"></i><span>" . $recht->title . "</span></h4>";
         echo "\t\t<div class=\"collapse\" id=\"" . $id . "\" data-parent=\"#" . $parent . "\">";
-        echo "\t\t<p class=\"collapse-content\">" . $recht->description . "</p>";
+        echo "\t\t" . $content;
         echo "\t</div>";
         echo "</div>";
     }
