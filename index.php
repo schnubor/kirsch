0535f254e07789f5388387bd4999c23059be7498
schnubor <info@chko.org>
2019-03-08 16:25:46 +0100

    fixed routing to remove any queries from url

--- index.php
+++ index.php
@@ -9,14 +9,16 @@ $less = new \lessc;
 $less->compileFile( LESS_DIR . '/main.less', __DIR__ . '/assets/css/compiled.css' );
 
 $rules = array( 
-    'impressum'   => "/impressum",        // '/impressum'
-    'datenschutz' => "/datenschutz",    // '/datenschutz'
-    'home'        => "/"                // '/'
+    'impressum'   => '/impressum',      // '/impressum'
+    'datenschutz' => '/datenschutz',    // '/datenschutz'
+    'home'        => '/',               // '/'
 );
 
 $uri = rtrim( dirname($_SERVER["SCRIPT_NAME"]), '/' );
 $uri = '/' . trim( str_replace( $uri, '', $_SERVER['REQUEST_URI'] ), '/' );
 $uri = urldecode( $uri );
+$uri = preg_replace('/\/\?.*/', '', $uri);
+$uri = $uri ? $uri : '/';
 
 foreach ( $rules as $action => $rule ) {
     if ( preg_match( '~^'.$rule.'$~i', $uri, $params ) ) {
